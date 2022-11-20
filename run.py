e365edd6d13df1d6ff6ab841df99d52db73239af
pyCabba <106534212+pyCabba@users.noreply.github.com>
2022-11-20 14:26:32 +0400

    Create run.py
    This script to process the text files from the supplier-data/descriptions directory

---
+++ run.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+import os
+import requests
+
+fruits = {}
+keys = ["name", "weight", "description", "image_name"]
+index = 0
+path = "./supplier-data/descriptions/"
+img_path = "./supplier-data/images/"
+for file in os.listdir("./supplier-data/descriptions"):
+    with open(path + file) as f:
+        for ln in f:
+            line = ln.strip()
+            if "lbs" in line:
+                nline = line.split()
+                wght = int(nline[0])
+                fruits["weight"] = wght
+                index += 1
+            else:
+                try:
+                    fruits[keys[index]] = line
+                    index += 1
+                except:
+                    fruits[keys[2]] = line
+        index = 0
+        split_f = file.split(".")
+        name = split_f[0] + ".jpeg"
+        for fle in os.listdir("./supplier-data/images"):
+            if fle == name:
+                fruits["image_name"] = name
+        response = requests.post("http://<External_IP>/fruits/", json=fruits) #You need to put your External IP
+        fruits.clear()
